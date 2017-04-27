fc5210125e7f6429a40202d6af8d94f09477b1dc
Michael <mulshine@princeton.edu>
2017-04-27 18:12:57 -0400

    master

--- plork_push.sh
+++ plork_push.sh
@@ -1,5 +1,5 @@
 find ./Patches/ -name "*.zip" -type f|xargs rm -f;
 for i in ./Patches/*; do zip -r "${i%/}.zip" "$i"; done
-git add --all;
-git -am $2;
-git push origin $1;
+git add --all
+git commit -m $1
+git push origin $2
