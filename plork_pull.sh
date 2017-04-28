7d4ac020baca8cc82a62e860df1bf29ca5c35dca
Michael <mulshine@princeton.edu>
2017-04-27 23:19:43 -0400

    PLOrk repo update: 27/04/2017 23:19:42

--- plork_pull.sh
+++ plork_pull.sh
@@ -1,5 +1,7 @@
-#cd ~/PLOrk
 git stash
 git pull origin master
 cd ./Patches;
-for i in *.zip; do unzip "${i%/}" -d "${i%.*}"; done
+rm -rf */;
+for i in *.zip; do echo "${i%/}" && echo "${i%.*}" && unzip "${i%/}" -d "."; done
+
+cd ..;
