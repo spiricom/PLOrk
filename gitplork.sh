634443194af528ed7a976bb2fd8dbbaf79b68227
Michael <mulshine@princeton.edu>
2017-04-27 23:21:48 -0400

    Removed useless files.

--- gitplork.sh
+++
@@ -1,8 +0,0 @@
-dt=$(date '+%d/%m/%Y %H:%M:%S');
-mess='PLOrk repo update: ';
-mess+="$dt";
-find ./Patches/ -name "*.zip" -type f|xargs rm -f;
-for i in ./Patches/*; do zip -r "${i%/}.zip" "$i"; done
-git add --all
-git commit -m "$mess"
-git push origin $1
